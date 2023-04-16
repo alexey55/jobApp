1f74e3b13d6641f060ed46b4001f253a5c2c6e54
alexey <alexey@mail.ru>
2023-04-16 16:50:33 +0300

    empty project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { Stack, Button } from "react-bootstrap";
+
+function App() {
+  return (
+    
+      <Stack direction="horizontal" gap={2}>
+  <Button as="a" variant="primary">
+    Button as link
+  </Button>
+  <Button as="a" variant="success">
+    Button as link
+  </Button>
+</Stack>
+    
+  );
+}
+
+export default App;
